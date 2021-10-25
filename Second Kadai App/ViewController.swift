2cc31fa7a4a4e8a2a94ee9f5a43cb1b85d706147
MARUYAMA GOHTA <maruyama.g.5@gmail.com>
2021-10-26 08:25:41 +0900

    Initial Commit

---
+++ Second Kadai App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Second Kadai App
+//
+//  Created by 丸山昂大 on 2021/10/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
